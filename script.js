95bd399c3a3de204a5489825f4f61e2bf35460e5
Pedini Jayashree <134771316+pedinistar@users.noreply.github.com>
2023-11-11 21:15:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var tl = gsap.timeline({
+  repeat: -1,
+});
+
+tl.to(
+  ".image-container",
+  {
+    ease: Expo.easeInOut,
+    width: "100%",
+    duration: 2,
+    stagger: 2,
+  },
+  "a"
+)
+  .to(
+    ".text h1",
+    {
+      ease: Expo.easeInOut,
+      duration: 2,
+      stagger: 2,
+      top: 0,
+    },
+    "a"
+  )
+  .to(
+    ".text h1",
+    {
+      delay: 2,
+      ease: Expo.easeInOut,
+      stagger: 2,
+      top: "-100%",
+    },
+    "a"
+  );
